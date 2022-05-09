4967bc5c277367e17a99aea8659622b5e6be7f7b
NataliaPatsey <patsey_natasha@mail.ru>
2022-05-10 00:59:15 +0300

    lab1: templates

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,10 @@
-from blog_app.views import index, page
+from blog_app.views import index, page, static_page, based_page2, based_page3
 from django.urls import path, include
 
 urlpatterns = [
-    path('',index),
-    path('page/',page),
+    path('', index),
+    path('page/', page),
+    path('page1/', static_page),
+    path('page2/', based_page2),
+    path('page3/', based_page3),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,6 +3,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def based_page2(request):
+    return render(request, 'blog_app/page2.html')
+
+
+def based_page3(request):
+    data = dict()
+    data['tel'] = '+ 375-29-1234567'
+    data['address'] = 'Minsk, Mir, 1'
+    return render(request, 'blog_app/page3.html', {'data': data})
+
+
+def static_page(request):
+    return render(request, 'blog_app/page1.html')
+
+
 def index(request):
     return HttpResponse('<h2>Главная</h2>')
 
