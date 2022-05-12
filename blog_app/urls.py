f6d8e815313bdb628a9134c2d26897b4e0a905b3
NataliaPatsey <patsey_natasha@mail.ru>
2022-05-12 17:30:21 +0300

    lab2:static files and urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', index),
     path('page/', page),
-    path('page1/', static_page),
-    path('page2/', based_page2),
-    path('page3/', based_page3),
+    path('page1/', static_page, name='static_page'),
+    path('page2/', based_page2, name='based_page2'),
+    path('page3/', based_page3, name='based_page3'),
 ]
